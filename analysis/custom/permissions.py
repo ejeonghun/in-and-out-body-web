65722f23289ba16cfdf4931197cd644d2d56eb5f
ejeonghun <wjdgns4019@gmail.com>
2025-01-24 11:06:00 +0900

    기능 변경 - 스웨거 IP 허용 로직 변경

--- analysis/custom/permissions.py
+++ analysis/custom/permissions.py
@@ -9,21 +9,20 @@ logger = logging.getLogger(__name__)
 # 허용된 IP 주소만 접근 가능한 Permission + 로깅
 class IsAllowedIP(BasePermission):
     def has_permission(self, request, view):
-        ip = request.META.get('REMOTE_ADDR')
-        path = request.path  # 요청 경로
-        response_code = 404  # 응답 코드
+        # X-Forwarded-For 헤더에서 실제 클라이언트 IP 가져오기
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0].strip()
+        else:
+            ip = request.META.get('REMOTE_ADDR')
 
-        # IP 주소가 192.168.0.1 ~ 192.168.0.254 범위에 있는지 확인 - 사내 내부 IP
-        if ip.startswith('192.168.0.') and 1 <= int(ip.split('.')[-1]) <= 254:
-            return True
-        # Docker 내부 IP, 프로메테우스 172.21.0.2
-        elif ip.startswith('172.21.0.') and 1 <= int(ip.split('.')[-1]) <= 254:
-            return True
+        path = request.path
+        response_code = 404
 
-        elif ip == '14.46.152.143':  # 회사 공인IP
-            return True
-
-        elif ip == '127.0.0.1':  # 테스트용 로컬 IP는 허용
+        # IP 체크 로직
+        if (ip.startswith('192.168.0.') and 1 <= int(ip.split('.')[-1]) <= 254) or \
+                (ip.startswith('172.21.0.') and 1 <= int(ip.split('.')[-1]) <= 254) or \
+                ip in ['14.46.152.143', '127.0.0.1']:
             return True
 
         # 허용되지 않은 IP일 경우 로깅 및 404 Page not found 에러 발생
