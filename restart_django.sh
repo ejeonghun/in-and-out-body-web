c87c49a10b4752cf0f67dd1b8f5280d22b303e55
ejeonghun <wjdgns4019@gmail.com>
2025-02-19 14:21:36 +0900

    기능 수정 - CI/CD 셸 수정

--- restart_django.sh
+++ restart_django.sh
@@ -4,11 +4,18 @@ if [ -f .env ]; then
     export $(grep -v '^#' .env | xargs)
 fi
 
-# Kill existing Gunicorn process
-PID=$(pgrep -f "gunicorn.*:44561")
-if [ -n "$PID" ]; then
-    echo "Stopping Gunicorn process: $PID"
-    kill -9 $PID
+# Gunicorn 프로세스 종료
+GUNICORN_PID=$(pgrep -f "gunicorn.*:44561")
+if [ -n "$GUNICORN_PID" ]; then
+    echo "Stopping Gunicorn process: $GUNICORN_PID"
+    kill -9 $GUNICORN_PID
+fi
+
+# mail_fetch_thread.py 프로세스 종료
+MAIL_FETCH_PID=$(pgrep -f "mail_fetch_thread.py")
+if [ -n "$MAIL_FETCH_PID" ]; then
+    echo "Stopping mail_fetch_thread.py process: $MAIL_FETCH_PID"
+    kill -9 $MAIL_FETCH_PID
 fi
 
 # 운영 환경일 때만 collectstatic
@@ -17,5 +24,14 @@ if [ "$ENVIRONMENT" == "prod" ] || [ "$ENVIRONMENT" == "dev" ]; then
     echo "yes" | python manage.py collectstatic > /dev/null 2>&1
 fi
 
+
+# mail_fetch_thread.py를 백그라운드에서 실행하고 PID를 저장
+echo "Starting mail_fetch_thread.py..."
+python /path/to/mail_fetch_thread.py &  # &를 사용하여 백그라운드에서 실행
+MAIL_FETCH_PID=$!
+echo "mail_fetch_thread.py PID: $MAIL_FETCH_PID"
+
+
+# Gunicorn 시작
 echo "Starting Gunicorn... port : 44561"
 gunicorn --bind 0.0.0.0:44561 mysite.wsgi:application --daemon
