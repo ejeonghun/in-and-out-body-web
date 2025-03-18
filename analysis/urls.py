8f8cc894a93598c4de9ccfa1ef763f4184985914
ejeonghun <wjdgns4019@gmail.com>
2025-03-18 16:48:50 +0900

    스웨거 IP 필터링 해제

--- analysis/urls.py
+++ analysis/urls.py
@@ -28,8 +28,8 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
-    # permission_classes=(permissions.AllowAny,),
-    permission_classes=(IsAllowedIP,),  # 허용된 IP주소만 접근 가능
+    permission_classes=(permissions.AllowAny,),
+    # permission_classes=(IsAllowedIP,),  # 허용된 IP주소만 접근 가능
 )
 
 urlpatterns = [
